fc4969e08199d39fb8b3038dda16d6a2e12c6e81
Sangeet16 <saha.san@northeastern.edu>
2024-03-20 19:26:01 -0400

    Sangeet/tfinfra (#13)
    * Terraform for vpc
    
    * added workflow
    
    * removed terraform apply
    
    * fixed build.yml
    
    * checking workflow failure
    
    * Updated gitignore
    
    * removed lock file
    
    * default routes handled
    
    * added firewall
    
    * changed workflow
    
    * changed workflow
    
    * testing
    
    * testing
    
    * changed to DEV
    
    * test
    
    * testing
    
    * Update main.tf
    
    * added cloud instance
    
    * file fix
    
    * added logging and new service account
    
    * added logging and new service account
    
    * minor fix
    
    ---------
    
    Co-authored-by: Sangeet <sangeetsaha1116@gmail.com>

--- main.tf
+++ main.tf
@@ -6,6 +6,17 @@ provider "google" {
   region      = var.region
 }
 
+
+resource "random_password" "password" {
+  length           = 8
+  special          = false
+}
+
+resource "random_string" "user_name" {
+  length           = 6
+  special          = false
+}
+
 # Create VPC
 
 resource "google_compute_network" "test_vpc_network" {
@@ -32,6 +43,7 @@ resource "google_compute_subnetwork" "db_subnet" {
   ip_cidr_range = var.db_subnet_cidr
   region        = var.region
   network       = google_compute_network.test_vpc_network.id
+  private_ip_google_access = true
   depends_on = [google_compute_network.test_vpc_network]
 }
 
@@ -57,10 +69,117 @@ resource "google_compute_firewall" "webapp_firewall" {
   depends_on = [google_compute_subnetwork.webapp_subnet]
 }
 
+resource "google_compute_global_address" "default" {
+  project      = google_compute_network.test_vpc_network.project
+  name         = "private-google-access-ip"
+  address_type = "INTERNAL"
+  purpose      = "VPC_PEERING"
+  prefix_length = 24
+  network      = google_compute_network.test_vpc_network.id
+  # address      = var.computeGlobalAddress
+  depends_on = [google_compute_network.test_vpc_network]
+}
+
+resource "google_service_networking_connection" "default" {
+  network                 = google_compute_network.test_vpc_network.id
+  service                 = "servicenetworking.googleapis.com"
+  reserved_peering_ranges = [google_compute_global_address.default.name]
+  depends_on = [google_compute_global_address.default]
+}
+
+resource "google_sql_database_instance" "cloudsql_instance" {
+  name             = "your-cloudsql-instance-name"
+  project          = var.project_id
+  region           = var.region
+  database_version = "MYSQL_8_0"
+
+  settings {
+    tier = "db-custom-1-3840"
+    disk_size = var.disk_size
+    disk_type = var.disk_type
+    availability_type = var.availability_type
+    ip_configuration {
+      ipv4_enabled    = var.ipv4_enabled
+      private_network = google_compute_network.test_vpc_network.id
+    }
+    backup_configuration {
+      enabled = true
+      binary_log_enabled = true
+    }
+  }
+  deletion_protection = var.deletion_protection
+
+  depends_on = [google_compute_global_address.default, google_service_networking_connection.default]
+}
+
+resource "google_sql_database" "testdb" {
+  name     = "webapp"
+  instance = google_sql_database_instance.cloudsql_instance.name
+  depends_on = [google_sql_database_instance.cloudsql_instance]
+}
+
+resource "google_sql_user" "test_user" {
+  name     = join("-", ["test",random_string.user_name.result])
+  instance = google_sql_database_instance.cloudsql_instance.name
+  password = random_password.password.result
+  depends_on = [google_sql_database_instance.cloudsql_instance]
+}
+
+resource "google_service_account" "logging_service_account" {
+  account_id   = "logging-service-account"
+  display_name = "Logging Service Account"
+}
+
+resource "google_project_iam_binding" "logging_admin_binding" {
+  project = var.project_id
+  role    = "roles/logging.admin"
+  members = [
+    "serviceAccount:${google_service_account.logging_service_account.email}",
+  ]
+  depends_on = [google_service_account.logging_service_account]
+}
+
+resource "google_project_iam_binding" "metric_writer_binding" {
+  project = var.project_id
+  role    = "roles/monitoring.metricWriter"
+  members = [
+    "serviceAccount:${google_service_account.logging_service_account.email}",
+  ]
+  depends_on = [google_project_iam_binding.logging_admin_binding]
+}
 
 module "compute" {
   source = "./compute"
+  zone = var.zone
   subnetwork_name = google_compute_subnetwork.webapp_subnet.self_link
+  db_name = google_sql_database.testdb.name
+  db_user = google_sql_user.test_user.name
+  db_password = google_sql_user.test_user.password
+  db_ip = google_sql_database_instance.cloudsql_instance.private_ip_address
+  service_account = google_service_account.logging_service_account.email
+  depends_on = [google_sql_user.test_user, google_sql_database.testdb, google_sql_database_instance.cloudsql_instance,
+    google_project_iam_binding.metric_writer_binding]
+}
+
+locals {
+  vm_ip_address = module.compute.vm_ip_address
+  depends_on = [module.compute]
+}
+
+# resource "google_dns_managed_zone" "my_zone" {
+#   name        = "my-zone"
+#   dns_name    = "paracloud.site."
+#   description = "Managed by Terraform"
+#   depends_on = [module.compute]
+# }
+
+resource "google_dns_record_set" "my_record" {
+  name    = "paracloud.site."
+  type    = "A"
+  ttl     = 300
+  managed_zone = "sangeet-dns-zone"
+  rrdatas = [local.vm_ip_address]
+  depends_on =  [module.compute]
 }
 
 
